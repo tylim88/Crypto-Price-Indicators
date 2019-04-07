dc0bce8d65fb2b960675780e14db35fc4664055b
unknown <limteikyee@gmail.com>
2019-04-08 00:57:37 +0800

    add search field into app bar

--- src/components/Tab/SimpleTabs.js
+++ src/components/Tab/SimpleTabs.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
+import { fade } from '@material-ui/core/styles/colorManipulator'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import InputBase from '@material-ui/core/InputBase'
 import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Typography from '@material-ui/core/Typography'
 import EnhancedTable from '../Table/EnhancedTable'
 import { tableContainer } from '../../state'
-
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
 function TabContainer(props) {
 	return (
 		<Typography component='div' style={{ padding: 8 * 3 }}>
@@ -24,6 +29,50 @@ const styles = theme => ({
 	root: {
 		flexGrow: 1,
 		backgroundColor: theme.palette.background.paper,
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit,
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		width: theme.spacing.unit * 9,
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%',
+	},
+	inputInput: {
+		paddingTop: theme.spacing.unit,
+		paddingRight: theme.spacing.unit,
+		paddingBottom: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit * 10,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: 120,
+			'&:focus': {
+				width: 200,
+			},
+		},
 	},
 })
 
@@ -47,13 +96,28 @@ class SimpleTabs extends React.Component {
 		return (
 			<div className={classes.root}>
 				<AppBar position='static'>
-					<Tabs value={value} onChange={this.handleChange}>
-						<Tab label='✩ Favourite' />
-						<Tab label='BNB Markets' />
-						<Tab label='BTC Markets' />
-						<Tab label='ALTS Markets' />
-						<Tab label='USDⓈ Markets' />
-					</Tabs>
+					<Toolbar>
+						<Tabs value={value} onChange={this.handleChange}>
+							<Tab label='✩ Favourite' />
+							<Tab label='BNB Markets' />
+							<Tab label='BTC Markets' />
+							<Tab label='ALTS Markets' />
+							<Tab label='USDⓈ Markets' />
+						</Tabs>
+						<div className={classes.grow} />
+						<div className={classes.search}>
+							<div className={classes.searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								placeholder='Search…'
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput,
+								}}
+							/>
+						</div>
+					</Toolbar>
 				</AppBar>
 
 				{value === 0 && (
